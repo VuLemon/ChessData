201bb6eea436cd3b0edfe93a8bca89f2eca217ed
VuLemon <vule@brandeis.edu>
2024-07-17 18:44:13 -0400

    Added legal move highlighting

--- frontend/scripts/gameLogic.js
+++ frontend/scripts/gameLogic.js
@@ -1,12 +1,41 @@
 var board = null
 var game = new Chess()
 
+var highlightedWhite = '#a9a9a9'
+var highlightedBlack = '#696969'
+
+function highlighted(square) {
+  var $square = $("#myBoard .square-" + square)
+  console.log("#myBoard .square-" + square)
+  var background = highlightedWhite
+  if ($square.hasClass('black-3c85d')) {
+    background = highlightedBlack
+  }
+  $square.css("background", background)
+}
+
+function revertHighlighted() {
+  $('#myBoard .square-55d63').css('background', '')
+  console.log("revertHighlighted fired")
+}
+
 function onDragStart(source, piece) {
     if (game.game_over()) return false
     if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
     (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-    return false
-}}
+    return false}
+    revertHighlighted()
+    var moves = game.moves({
+      square: source,
+      verbose: true
+    })
+    for (var i = 0; i < moves.length; i++) {
+      highlighted(moves[i].to)
+    }
+
+
+    
+}
 
 function onDrop (source, target) {
     // see if the move is legal
@@ -15,9 +44,11 @@ function onDrop (source, target) {
       to: target,
       promotion: 'q' // NOTE: always promote to a queen for example simplicity
     })
-  
+    
     // illegal move
     if (move === null) return 'snapback'
+    revertHighlighted()
+
   
   }
 function onSnapEnd () {
